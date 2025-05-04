6ba3613c6b4ba035fec69bf33e30c8593f049caf
nguyenminhquan004 <phanhuonghn78@gmail.com>
2025-05-04 23:21:33 +0700

    Add role-based access control middleware to authorize user roles

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,11 @@
+// Phân quyền truy cập cho người dùng dựa trên vai trò của họ
+exports.authorizeRoles = (role) => {
+    return (req, res, next) => {
+        if (!req.user || req.user.role !== role) {
+            return res
+                .status(403)
+                .json({ message: "Bạn không có quyền truy cập!" });
+        }
+        next();
+    };
+};
