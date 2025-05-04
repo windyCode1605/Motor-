1b2456d536036cc1224c37a404913762642065d8
nguyenminhquan004 <phanhuonghn78@gmail.com>
2025-05-04 23:19:24 +0700

    Add createReservation function to handle reservation creation

---
+++ controllers/reservationController.js
@@ -0,0 +1,47 @@
+const db = require("../config/db");
+
+exports.createReservation = (req, res) => {
+    const {
+        customer_id,
+        car_id,
+        start_date,
+        end_date,
+        status,
+        total_price,
+        payment_status,
+        pickupLocation,
+        returnLocation,
+    } = req.body;
+
+    const sql = `INSERT INTO reservations 
+    (customer_id, car_id, start_date, end_date, status, total_price, payment_status, pickup_location, return_location)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    db.query(
+        sql,
+        [
+            customer_id,
+            car_id,
+            start_date,
+            end_date,
+            status,
+            total_price,
+            payment_status,
+            pickupLocation,
+            returnLocation,
+        ],
+        (err, result) => {
+            if (err) {
+                return res
+                    .status(500)
+                    .json({ message: "Khong the tao don dat xe 🤦‍♂️" });
+            } else {
+                res.status(201).json({
+                    success: true,
+                    message: "Tao don dat xe thanh cong 🤷‍♂️",
+                    revervation_id: result.insertId,
+                });
+            }
+        }
+    );
+};
