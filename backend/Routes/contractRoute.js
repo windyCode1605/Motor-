24cb83c4ee8f9e5715af8cb01f4ff178d83e2b74
nguyenminhquan004 <phanhuonghn78@gmail.com>
2025-05-30 13:06:52 +0700

    login

--- backend/Routes/contractRoute.js
+++ backend/Routes/contractRoute.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const contractController = require('../controllers/contractController');
-const { authenticateToken } = require('../middleware/authMiddleware');
-const { authorizeRoles } = require('../middleware/roleMiddleware');
+const { getAllContracts, getContractById, createContract, updateContract, deleteContract } = require('../controllers/contractController');
 
-// Middleware for admin and staff only
-
-
-// Lấy tất cả hợp đồng
-router.get('/', contractController.getAllContracts);
-
-
-// Cập nhật hợp đồng
-router.put('/:id', contractController.updateContract);
-
-// Xóa hợp đồng
-router.delete('/:id', contractController.deleteContract);
+router.get('/getAll', getAllContracts);
+router.get('/getById/:id', getContractById);
+router.post('/create', createContract);
+router.put('/update/:id', updateContract);
+router.delete('/delete/:id', deleteContract);
 
 module.exports = router;
